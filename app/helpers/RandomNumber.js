f9ce27d8eed84d03a257baa93e144757ce7ed77b
Raul-TC <raultc.9819@gmail.com>
2022-03-13 01:05:19 -0600

    fix bug clic

--- app/helpers/RandomNumber.js
+++ app/helpers/RandomNumber.js
@@ -3,24 +3,23 @@ import { fetchRequest } from "./fetchRequest.js";
 
 export async function RandomNumber() {
   document.addEventListener("click", async (e) => {
-    if (e.target.matches(".dice *")) {
+    if (e.target.matches(".dice *") || e.target.matches(".dice")) {
+      let random = Math.floor(Math.random() * 224);
+      document.querySelector(".dice img").classList.remove("animation");
+      setTimeout(() => {
+        document.querySelector(".dice img").classList.add("animation");
+      }, 300);
+      localStorage.setItem("id", `${random}`);
+      await fetchRequest({
+        url: `https://api.adviceslip.com/advice/${random}`,
+        cbSuccess: (advice) => {
+          document.querySelector(".containerInfo").innerHTML = null;
+          document.querySelector(
+            ".containerInfo"
+          ).innerHTML = `    <h1>ADVICE #${advice.slip.id}</h1>
+      <p>"${advice.slip.advice}"</p>`;
+        },
+      });
     }
-    let random = Math.floor(Math.random() * 224);
-    document.querySelector(".dice img").classList.remove("animation");
-    setTimeout(() => {
-      document.querySelector(".dice img").classList.add("animation");
-    }, 300);
-    localStorage.setItem("id", `${random}`);
-
-    await fetchRequest({
-      url: `https://api.adviceslip.com/advice/${random}`,
-      cbSuccess: (advice) => {
-        document.querySelector(".containerInfo").innerHTML = null;
-        document.querySelector(
-          ".containerInfo"
-        ).innerHTML = `    <h1>ADVICE #${advice.slip.id}</h1>
-    <p>"${advice.slip.advice}"</p>`;
-      },
-    });
   });
 }
